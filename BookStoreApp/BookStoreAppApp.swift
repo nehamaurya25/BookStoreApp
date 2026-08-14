5804045b97c304464f8771ae071050d7e4faf935
Neha Maurya <neha.maurya@ext.aircanada.ca>
2026-08-14 10:59:41 +0530

    Initial Commit

---
+++ BookStoreApp/BookStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreAppApp.swift
+//  BookStoreApp
+//
+//  Created by Neha Maurya on 14/08/26.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
